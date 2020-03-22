e0c7849eac5d046858d96a562368140439cb16df
Ian Kelly <IanKelly134@gmail.com>
2020-03-22 17:06:01 -0600

    March 22nd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  API_URL: "http://localhost:50438/api/"
+  API_URL: "http://bittlebattlebaseballwebapi.azurewebsites.net/api/"
+  // API_URL: "http://localhost:50438/api/"
 };
 
 /*
