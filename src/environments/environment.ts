3c9e217a2560dee48c85d826d61736e7e39ff29d
Ian Kelly <IanKelly134@gmail.com>
2024-05-17 11:20:29 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   //API_URL: "https://localhost:5001/api/"
   API_URL: "https://bittlebattlebaseballwebapi.onrender.com/api/"
 };
